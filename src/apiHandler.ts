9cd1890916f33ad40af3c85693a336f241890fb1
Pau <pauguvi@gmail.com>
2024-04-10 00:06:30 +0200

    change API url to production url

--- src/apiHandler.ts
+++ src/apiHandler.ts
@@ -1,7 +1,9 @@
 import { PackageType } from "./@types/types";
 
+const API_URL = 'https://npm-higher-lower-api.onrender.com';
+
 export const getPackages = async (): Promise<string[]> => {
-  const response = await fetch('http://localhost:5000/packages?count=10');
+  const response = await fetch(API_URL + '/packages?count=10');
   if (response.ok) {
     const data = await response.json();
     return data as string[];
@@ -12,7 +14,7 @@ export const getPackages = async (): Promise<string[]> => {
 }
 
 export const getPackage = async (name: string): Promise<PackageType | null> => {
-  const response = await fetch(`http://localhost:5000/package/?` + new URLSearchParams({ name: name }));
+  const response = await fetch(API_URL + '/package/?' + new URLSearchParams({ name: name }));
   if (response.ok) {
     const data = await response.json();
     return data as PackageType;
