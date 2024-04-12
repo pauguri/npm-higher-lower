7def2f3faf45d2746cec6e9e1244ec8041661390
Pau Guri <pauguvi@gmail.com>
2024-04-12 11:25:33 +0200

    add bg decoration

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 import './Home.css';
+import { decorPkgList } from '../decorPkgList';
 
 export default function Home({ loadingSetter: setLoading }: { loadingSetter: (loading: boolean) => void }) {
   const navigate = useNavigate();
@@ -13,13 +14,19 @@ export default function Home({ loadingSetter: setLoading }: { loadingSetter: (lo
     }, 500);
   }
 
+  const decorListOffset = Math.floor(Math.random() * decorPkgList.length);
+  const decorText = decorPkgList.substring(decorListOffset, decorListOffset + 700);
+
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen gap-12 p-4 md:p-8 lg:p-12 bg-dark-blue">
-      <div className='flex flex-col items-center gap-4 text-center'>
-        <h1 className='font-mono text-2xl font-bold md:text-4xl text-yellow'>npm-higher-lower</h1>
-        <p className='font-thin md:text-xl'>the game about guessing package popularity</p>
+    <div className="flex flex-col items-center justify-center w-screen h-screen p-4 overflow-hidden md:p-8 lg:p-12 bg-dark-blue">
+      <div className='bg-decor'>{decorText}</div>
+      <div className='relative z-10 flex flex-col items-center gap-12'>
+        <div className='flex flex-col items-center gap-4 text-center'>
+          <h1 className='font-mono text-2xl font-bold md:text-4xl text-yellow'>npm-higher-lower</h1>
+          <p className='font-thin md:text-xl'>the game about guessing package popularity</p>
+        </div>
+        <Button onClick={handleStartGame} className='bg-green'>Start Game</Button>
       </div>
-      <Button onClick={handleStartGame} className='bg-green'>Start Game</Button>
     </div>
   )
 }
